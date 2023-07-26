f9404a852299d7d052dd61c78500b0365f063686
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-07-26 15:41:07 +0300

    Add custom setter method to student

--- student.rb
+++ student.rb
@@ -1,15 +1,18 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, **defaults)
     defaults[:name] ||= 'Unknown'
     defaults[:parent_permission] = true if defaults[:parent_permission].nil?
-
     super(age, **defaults)
+    self.classroom = classroom
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
