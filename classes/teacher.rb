9e752da4c1c235e14c9dc60cef1a18c05277440a
kifle23 <kiflekifle23@gmail.com>
2023-08-01 14:42:27 +0300

    Update order of initializer for Teacher class

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -4,7 +4,7 @@ require './classes/person'
 class Teacher < Person
   attr_accessor :specialization, :id
 
-  def initialize(age, specialization, **defaults)
+  def initialize(specialization, age, **defaults)
     @id = rand(30..5000)
     defaults[:name] ||= 'Unknown'
     defaults[:parent_permission] = true if defaults[:parent_permission].nil?
