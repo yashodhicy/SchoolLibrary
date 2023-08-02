d4dbe33c8c8d49f3b07db6ee4cbe88d444887fe6
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-02 17:18:40 +0300

    Add test files for decorators

---
+++ tests/trimmer_decorator_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require_relative '../decorators/trimmer_decorator'  # Adjust the require path
+
+RSpec.describe TrimmerDecorator do
+  let(:nameable) { double('Nameable', correct_name: 'Long Name Here') }
+  subject(:decorator) { described_class.new(nameable) }
+
+  describe '#correct_name' do
+    context 'when the name is longer than 10 characters' do
+      it 'trims the name to a maximum of 10 characters' do
+        expect(decorator.correct_name).to eq('Long Name ')
+      end
+    end
+
+    context 'when the name is 10 characters or shorter' do
+      let(:nameable) { double('Nameable', correct_name: 'Short Name') }
+
+      it 'does not trim the name' do
+        expect(decorator.correct_name).to eq('Short Name')
+      end
+    end
+  end
+end
